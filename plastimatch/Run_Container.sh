066a13f7f561131210552436ca41f598b664d7c7
Haley Clark <gmail.com@hdeanclark>
2018-04-29 16:09:26 -0700

    Added plastimatch.
    This version is probably newer than the version you used during your PhD.

---
+++ plastimatch/Run_Container.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+image_basename="plastimatch"
+
+set -e 
+
+artifacts_dir="${HOME}/${image_basename}_container_artifacts/"
+mkdir -p "${artifacts_dir}"
+
+printf -- "${image_basename} container artifacts going to: ${artifacts_dir}\n" 1>&2
+
+sudo docker run \
+    -it \
+    --rm \
+    -v "${artifacts_dir}":/artifacts/:rw \
+    "${image_basename}":latest \
+
+#    -p 8080:80 \
+#    --entrypoint /bin/bash \
+
+
+rmdir "${artifacts_dir}" &>/dev/null || \
+    printf -- "${image_basename} container artifacts remain in ${artifacts_dir}\n" 1>&2
+
---
+++ plastimatch/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e 
+
+image_basename="plastimatch"
+
+commit_id=$(git rev-parse HEAD)
+
+clean_dirty="clean"
+sstat=$(git diff --shortstat)
+if [ ! -z "${sstat}" ] ; then
+    clean_dirty="dirty"
+fi
+
+build_datetime=$(date '+%Y%m%_0d-%_0H%_0M%_0S')
+
+#reporoot=$(git rev-parse --show-toplevel)
+#cd "${reporoot}"
+
+time sudo docker build \
+    --no-cache=true \
+    -t "${image_basename}":"built_${build_datetime}" \
+    -t "${image_basename}":"commit_${commit_id}_${clean_dirty}" \
+    -t "${image_basename}":latest \
+    -f ./Dockerfile \
+    .
+
