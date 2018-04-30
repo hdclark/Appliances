9f6c492f75b9b1b1b8b7ef72c3317e0112991adb
Haley Clark <gmail.com@hdeanclark>
2018-04-29 18:12:48 -0700

    Added rstats container with a smattering of packages.
    I basically selected packages at random. Prune the list as needed.

---
+++ r/Run_Default.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This script will act like a simplistic init process inside the container, setting up the runtime environment and
+# launching necessary programs. It is invoked by default, but can be overridden when launching the container.
+
+## Prepare the environment.
+#export XYZ="..."
+
+## Launch a dummy X server.
+#/usr/bin/Xorg \
+#  -noreset \
+#  +extension GLX \
+#  +extension RANDR \
+#  +extension RENDER \
+#  -depth 24 \
+#  -logfile ./xdummy.log \
+#  -config /etc/X11/xorg.conf \
+#  :1 &
+#xpid=$!
+#
+#sleep 5
+#export DISPLAY=:1
+
+# Run a service or perform an operation.
+R --version
+printf "\n\n\n"
+Rscript --version
+
+## Clean up, if necessary (usually not).
+#kill $xpid
+
