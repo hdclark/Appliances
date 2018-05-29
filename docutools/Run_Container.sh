aa1ea448f9145562e83b5dd344ef4966ebb3ee18
Hal <haley.clark@bccancer.bc.ca>
2018-05-29 10:25:32 -0700

    Pass-through pwd more easily.

--- docutools/Run_Container.sh
+++ docutools/Run_Container.sh
@@ -4,22 +4,15 @@ image_basename="docutools"
 
 set -e 
 
-artifacts_dir="${HOME}/${image_basename}_container_artifacts/"
-mkdir -p "${artifacts_dir}"
-
-printf -- "${image_basename} container artifacts going to: ${artifacts_dir}\n" 1>&2
-
 sudo docker run \
     -it \
     --rm \
-    -v "${artifacts_dir}":/artifacts/:rw \
+    -v "$(pwd)":/scratch/:rw \
+    -w /scratch/ \
     "${image_basename}":latest \
     $@
 
+#    -w /scratch/ \
 #    -p 8080:80 \
 #    --entrypoint /bin/bash \
-
-
-rmdir "${artifacts_dir}" &>/dev/null || \
-    printf -- "${image_basename} container artifacts remain in ${artifacts_dir}\n" 1>&2
 
