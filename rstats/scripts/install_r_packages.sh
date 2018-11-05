ae2fc44c23a585b1968d8254fc43810f169b627f
Hal <haley.clark@bccancer.bc.ca>
2018-11-05 13:26:04 -0800

    Add check to ensure rstats package installations succeed.

--- rstats/scripts/install_r_packages.sh
+++ rstats/scripts/install_r_packages.sh
@@ -3,18 +3,25 @@
 templibloc <- '/usr/local/lib/R/site-library'  # See '.libPaths()' output.
 dir.create(templibloc, showWarnings = FALSE, recursive = TRUE)
 
-for(package in c( 'relaimpo',                                                                   
-                  'modeltools',                                                                 
-                  'strucchange',                                                                
-                  'party',                                                                      
-                  'nlstools',                                                                   
-                  'tidyverse',                                                                  
-                  'readxl' )){                                                                  
+for(package in c( "relaimpo",                                                                   
+                  "modeltools",                                                                 
+                  "strucchange",                                                                
+                  "party",                                                                      
+                  "nlstools",                                                                   
+                  "tidyverse",                                                                  
+                  "readxl" )){                                                                  
     if(!require(package, character.only=T, quietly=T, warn.conflicts=F, lib.loc=templibloc)){   
         install.packages(package, repos='http://cran.rstudio.com/',                             
                                   dependencies=T,                                               
                                   verbose=F, quiet=T, lib=templibloc)                           
         library(package, character.only=T, quietly=T, verbose=F, lib.loc=templibloc)            
+
+        if(!require(package, character.only=T, quietly=T, warn.conflicts=F, lib.loc=templibloc)){
+            cat("Unable to install package. Terminating with non-zero exit status.\n")
+            quit(save="no", status=1)
+        }
     }                                                                                           
 }                                                                                               
+
+quit(save="no", status=0)
 
