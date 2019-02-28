7ee562d3b3f8b0bdbaf77c037d260d007b31c6c3
Hal <haley.clark@bccancer.bc.ca>
2019-02-28 14:03:20 -0800

    Added dictation transcoding and silence removal script.

---
+++ ffmpeg/Transcode_Audio.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get install -y sox
+
+in_file="in.opus"
+normalized_file="in_normalized.wav"
+silenced_file="in_silenced.wav"
+out_file="out.mp3"
+
+#NOISE_TOLERANCE="1dB"
+#ffmpeg -i "$in_file" -af silenceremove=0:0:0:-1:1:"${NOISE_TOLERANCE}" -ac 1 "$normalized_file" -y
+
+ffmpeg -i "$in_file" -filter:a loudnorm -ac 1 "$normalized_file" -y
+ffmpeg -i "$normalized_file" -filter:a volumedetect -f null /dev/null
+
+#sox "$normalized_file" "$silenced_file" silence -l 1 0.3 1% -1 2.0 1%
+sox "$normalized_file" "$silenced_file" silence 1 0.1 0.5% -1 0.1 0.5%
+
+ffmpeg -i "$silenced_file" -ac 1 "$out_file"
