7f135e23381ed5c323b553180fb5667a348b134e
Hal <haley.clark@bccancer.bc.ca>
2020-01-14 10:45:32 -0800

    Pylinac: make all-in-one plot.
    Also provided a more descriptive invocation script to simplify running the appliance.

--- pylinac/Analyze_TrajectoryLog.py
+++ pylinac/Analyze_TrajectoryLog.py
@@ -1,6 +1,10 @@
 
 from pylinac import load_log, MachineLogs
 
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
 path_to_folder = "/tmp/tlogs/"
 
 logs = MachineLogs(path_to_folder)
@@ -17,9 +21,38 @@ for log in logs:
     log.publish_pdf(filename=(plotbase + '_report.pdf'), metadata={'Number of beam holds': log.num_beamholds})
 
     # Produce specific plots.
-    log.axis_data.mu.save_plot_actual(filename=(plotbase + '_MU.png'))
-    log.axis_data.beam_hold.save_plot_actual(filename=(plotbase + '_beam_holds.png'))
-    #log.axis_data.gantry.save_plot_difference(filename=(plotbase + '_gantry.png'))
+    log.axis_data.beam_hold.save_plot_actual(filename=(plotbase + '_beam_holds.pdf'))
+    log.axis_data.mu.save_plot_actual(filename=(plotbase + '_MU.pdf'))
+    log.axis_data.gantry.save_plot_actual(filename=(plotbase + '_gantry.pdf'))
+    #log.axis_data.gantry.save_plot_difference(filename=(plotbase + '_gantry.pdf'))
+
+    # Produce a custom plot.
+    plt.close('all') # Reset
+
+
+    plt.subplot(3, 1, 1)
+    plt.title('Sampled Beam Holds, MUs, and Gantry Angles')
+    plt.plot( log.axis_data.beam_hold.actual, '-', linewidth=0.5)
+    #plt.xlabel('Temporal sample')
+    plt.xticks(color='w')
+    plt.ylabel('Beam Holds')
+    plt.grid()
+
+    plt.subplot(3, 1, 2)
+    plt.plot( log.axis_data.mu.actual, '-', linewidth=0.5)
+    #plt.xlabel('Temporal sample')
+    plt.xticks(color='w')
+    plt.ylabel('MU')
+    plt.grid()
+
+    plt.subplot(3, 1, 3)
+    plt.plot( log.axis_data.gantry.actual, '-', linewidth=0.5)
+    plt.xlabel('Temporal sample')
+    plt.ylabel('Gantry Angle')
+    plt.grid()
+
+    plt.savefig(plotbase + '_beam_holds_MU_and_gantry_angle.pdf')
+    plt.show()
 
 quit()
 
--- pylinac/Run_TrajectoryLog_Analysis.sh
+++ pylinac/Run_TrajectoryLog_Analysis.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+if [ ! -d /tmp/tlogs ] ; then
+    printf 'Create "/tmp/tlogs/" and place trajectory log files (*.bin) inside before proceeding.\n' 2>&1
+    exit 1
+fi
+
+logfiles=(/tmp/tlogs/*bin)
+if [[ ! -f "${logfiles[0]}" ]] ; then
+    printf 'Unable to locate binary trajectory log files (*.bin) in "/tmp/tlogs/".\n' 2>&1
+    exit 1
+fi
+
 ./Run_Container.sh python3 ./Analyze_TrajectoryLog.py
+
+printf 'Outputs will be found in "/tmp/tlogs/".\n'
 
