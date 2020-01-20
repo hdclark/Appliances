a8be2f7c40ba519ef2632820b6ab67ec647f975f
Hal <haley.clark@bccancer.bc.ca>
2020-01-20 13:13:00 -0800

    DICOM_anonymization: pass invocation parameters to anonymization scripts.

--- DICOM_anonymization/Run_anonymize_partial.sh
+++ DICOM_anonymization/Run_anonymize_partial.sh
@@ -4,11 +4,14 @@
 #
 # It will be run on all directories present.
 #
+# Note that all options will be passed through to the anonymization script, which will disable interactive polling for
+# the provided parameters.
+#
 find /artifacts/ \
   -mindepth 1 -maxdepth 1 \
   -type d \
   \! -iname '*_anon' \
   -exec printf "\n\nAnonymizing files in directory '{}'...\n" \; \
-  -exec anonymize_partial '{}' \; \
+  -exec anonymize_partial "$@" '{}' \; \
   -exec chown --recursive --reference='{}' '{}'_anon \;
 
--- DICOM_anonymization/build.sh
+++ DICOM_anonymization/build.sh
@@ -18,6 +18,7 @@ build_datetime=$(date '+%Y%m%_0d-%_0H%_0M%_0S')
 #cd "${reporoot}"
 
 time sudo docker build \
+    --network=host \
     --no-cache=true \
     -t "${image_basename}":"built_${build_datetime}" \
     -t "${image_basename}":"commit_${commit_id}_${clean_dirty}" \
