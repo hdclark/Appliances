e1129a3122aa6c88f048f26b7e13c4eece686faf
Haley Clark <gmail.com@hdeanclark>
2020-04-15 18:58:08 -0700

    Added minimal container for testing nix.

---
+++ nix/Run_Container.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+image_basename="halclark/nix"
+
+set -e 
+
+sudo docker run \
+    -it \
+    --rm \
+    --network=host \
+    -v "$(pwd)":/scratch/:rw \
+    -w /scratch/ \
+    "${image_basename}":latest \
+    $@
+
+#    -w /scratch/ \
+#    -p 8080:80 \
+#    --entrypoint /bin/bash \
+
---
+++ nix/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e 
+
+image_basename="halclark/nix"
+
+commit_id=$(git rev-parse HEAD)
+
+clean_dirty="clean"
+sstat=$(git diff --shortstat)
+if [ ! -z "${sstat}" ] ; then
+    clean_dirty="dirty"
+fi
+
+build_datetime=$(date '+%Y%m%_0d-%_0H%_0M%_0S')
+
+#reporoot=$(git rev-parse --show-toplevel)
+#cd "${reporoot}"
+
+time sudo docker build \
+    --no-cache=true \
+    --network=host \
+    -t "${image_basename}":"built_${build_datetime}" \
+    -t "${image_basename}":"commit_${commit_id}_${clean_dirty}" \
+    -t "${image_basename}":latest \
+    -f ./Dockerfile \
+    .
+
