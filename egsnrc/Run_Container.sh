f8dafb8b3bbe9ebdf9a75a09251d6dbb0b2bd8a5
Haley Clark <gmail.com@hdeanclark>
2020-05-12 18:08:42 -0700

    Added EGSnrc appliance.
    Two separate instances can be built: one using Debian Stable and one using CentOS 6.6 for compatibility with legacy applications.

---
+++ egsnrc/Run_Container.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+image_basename="egsnrc"
+
+set -e 
+
+sudo docker run \
+    -it \
+    --rm \
+    -v "$(pwd)":/scratch/:rw \
+    -w /scratch/ \
+    "${image_basename}":latest \
+    "$@"
+
---
+++ egsnrc/Run_Default_Simulation.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Launch a software X server capable of emulating OpenGL.
+/usr/bin/Xorg \
+  -noreset \
+  +extension GLX \
+  +extension RANDR \
+  +extension RENDER \
+  -depth 24 \
+  -logfile ./xdummy.log \
+  -config /etc/X11/xorg.conf \
+  :1 &
+xpid=$!
+
+sleep 5
+export DISPLAY=:1
+
+
+cd "$EGS_HOME"
+make
+egs_app -i slab.egsinp
+egs_view slab.egsinp slab.ptracks
+
+
+# Terminate the X server in case the container will be re-used.
+kill $xpid
+
