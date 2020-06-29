0451a0e880be4085e105be2d93b821dec2ee8fc1
Hal <haley.clark@bccancer.bc.ca>
2020-06-29 14:25:07 -0700

    Added a WIP pytorch appliance.

---
+++ pytorch/Run_Container.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+image_basename="pytorch"
+
+set -eu
+
+#export l_UID="$(id -u)"
+#export l_GID="$(id -g)"
+#export l_USER="$USER"
+
+sudo docker run \
+    -it \
+    --rm \
+    --network=host \
+    -v "$(pwd):$(pwd):rw" \
+    -v "/tmp/":"/tmp/":rw \
+    -w "$(pwd)" \
+    "${image_basename}":latest \
+    $@
+
+    #-v         "...":"/usr/local/lib/python3.7/dist-packages/pytorch/demo_files/Tlog.bin":ro \
+    #-v "/tmp/in.dlg":"/usr/local/lib/python3.7/dist-packages/pytorch/demo_files/AQA.dlg":ro \
+    #--user $l_UID:$l_GID \
+    #--workdir="/home/$l_USER" \
+    #--volume="/etc/group:/etc/group:ro" \
+    #--volume="/etc/passwd:/etc/passwd:ro" \
+    #--volume="/etc/shadow:/etc/shadow:ro" \
+
+
---
+++ pytorch/Test.py
@@ -0,0 +1,10 @@
+
+import matplotlib
+import numpy
+import PIL
+import shapely
+import torch
+
+dummy = 0.0
+
+
